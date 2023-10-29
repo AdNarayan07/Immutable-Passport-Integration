88b60df27accee6710ed0bc3f5125051aa30e1fa
Val Luminarias <vluminarias@yahoo.com>
2023-10-29 16:46:14 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <main class="w-full min-h-screen bg-teal-600">
+    
+    <div class="pt-32 mx-auto max-w-md flex items-center justify-center">
+
+      <button type="button" class="rounded-md bg-teal-800 px-4 py-2 text-teal-100 hover:bg-teal-900 shadow-lg border-b-4 border-b-teal-900">
+        Login
+      </button>
+      
+    </div>
+      
+  </main>
+</template>
+
+<style scoped>
+</style>
